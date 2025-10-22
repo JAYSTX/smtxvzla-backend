754f0852f31d102c7f7b3591268f3fc84034c54f
SmartX <smartxp2p@gmail.com>
2025-10-22 18:16:15 +0200

    Update p2p.ts

--- src/routes/p2p.ts
+++ src/routes/p2p.ts
@@ -1,9 +1,10 @@
 // src/routes/p2p.ts
 import { FastifyInstance } from "fastify";
-import { createOrder, listOrders, acceptOrder } from "../controllers/p2pController.js";
+import { createOrder, listOrders, acceptOrder, releaseOrder } from "../controllers/p2pController.js";
 
 export async function p2pRoutes(app: FastifyInstance) {
-  app.post("/p2p/create", { onRequest: [app.authenticate] }, createOrder);
-  app.get("/p2p/orders", listOrders);
-  app.post("/p2p/accept/:id", { onRequest: [app.authenticate] }, acceptOrder);
+  app.post("/create", { onRequest: [app.authenticate] }, createOrder);
+  app.get("/orders", listOrders);
+  app.post("/accept/:id", { onRequest: [app.authenticate] }, acceptOrder);
+  app.post("/release/:id", { onRequest: [app.authenticate] }, releaseOrder);
 }
